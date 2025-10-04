5225e72b7c27f48b8389fc365f1709178a7630b2
jschjo <jschjonneberg@gmail.com>
2025-10-04 18:00:27 +0200

    .

--- phpkurs/phpkursexample.php
+++
@@ -1,8 +0,0 @@
-<?php    /* Eksempel 1 */
-/*
-/*    Programmet mottar fra et HTML-skjema et fornavn og et etternavn ved POST-metoden
-/*    Programmet skriver ut en "god dag"-melding med personens navn 
-*/
-
-  print ("God dag <br />");  
-?>
