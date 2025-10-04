eb7d9ee9329dace5d18d3c80dd1375038fc44062
jschjo <jschjonneberg@gmail.com>
2025-10-04 17:10:50 +0200

    -

--- tema01/oppgave1.php
+++ tema01/oppgave1.php
@@ -1,13 +1,10 @@
-<?php    /* Eksempel 1 */
+<?php    /* Oppgave 1*/
 /*
 /*    Programmet mottar fra et HTML-skjema et fornavn og et etternavn ved POST-metoden
 /*    Programmet skriver ut en "god dag"-melding med personens navn 
 */
-
-
   $fornavn=$_POST ["fornavn"];
   $etternavn=$_POST ["etternavn"];  
- 
+	
   print ("God dag $fornavn $etternavn <br />");  
-
 ?>
