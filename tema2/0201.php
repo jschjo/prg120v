bf0a84105e7b0e7568a82a6d8cc2b606bc145990
jschjo <jschjonneberg@gmail.com>
2025-10-06 12:27:04 +0200

    .

---
+++ tema2/0201.php
@@ -0,0 +1,17 @@
+<?php    /* Oppgave 1*/
+/*
+/*    Programmet mottar fra et HTML-skjema et fornavn og et etternavn ved POST-metoden
+/*    Programmet skriver ut en "god dag"-melding med personens navn 
+*/
+  $tall1=$_POST ["tall1"];
+  $tall2=$_POST ["tall2"];  
+
+  $summen=$tall1 + $tall2;
+  $differansen=$tall1 - $tall2;
+
+    print ("Tall 1 er $tall1 <br />");
+    print ("Tall 2 er $tall2 <br />");
+    print ("<br />");
+    print ("Summen er $summen <br />");
+    print ("Differansen er $differansen <br />");
+?>
