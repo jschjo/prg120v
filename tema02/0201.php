2b5dc32311030ecc024d6f171220fc4d3cf62295
jschjo <jschjonneberg@gmail.com>
2025-10-07 22:08:51 +0200

    .

--- tema02/0201.php
+++ tema02/0201.php
@@ -4,10 +4,10 @@
 /*    Programmet skriver ut en "god dag"-melding med personens navn 
 */
   $tall1=$_POST ["tall1"];
-  $tall2=$_POST ["tall2"];  
+  $tall2=$_POST ["tall2"]; 
+  $tall3=$_POST ["tall3"]; 
 
   $summen=$tall1 + $tall2;
-  $differansen=$tall1 - $tall2;
 
     print ("Tall 1 er $tall1 <br />");
     print ("Tall 2 er $tall2 <br />");
