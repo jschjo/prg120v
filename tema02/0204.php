aa524f64eae106d1fe0738a2cf1b796dbd8eef02
jschjo <jschjonneberg@gmail.com>
2025-10-13 18:43:32 +0200

    .

--- tema02/0204.php
+++ tema02/0204.php
@@ -3,26 +3,26 @@
 /*    Programmet mottar fra et HTML-skjema et svar på spørsmålet "Hva er 3 ganger 3 ?"
 /*    Programmet sjekker om svaret er riktig og skriver ut en melding ang. svaret 
 */
-  $svar=$_POST ["svar"];
-  $svar2=$_POST ["svar"];
+  $gift=$_POST ["gift"];
+  $barn=$_POST ["barn"];
 	
-  if (!$svar || !$svar2)  
+  if (!$gift || !$barn  )  
     {
       print("Du har ikke svart p&aring; begge sp&oslash;rsm&aring;lene om du er gift og har barn ");
     }
-  else if ($svar == "j" && $svar2 == "j")  
+  else if ($gift == "j" && $barn == "j")  
     {
       print(" Du er gift og har barn");
     }   
-  else if ($svar == "j" && $svar2 == "n")  
+  else if ($gift == "j" && $barn == "n")  
     {
       print("Du er gift og har ikke barn");
     }
-  else if ($svar == "n" && $svar2 == "j")  
+  else if ($gift == "n" && $barn == "j")  
     {
       print("Du er ikke gift og har barn");
     }   
-  else if ($svar == "n" && $svar2 == "n")           
+  else     ($gift == "n" && $barn == "n")           
     {
       print("Du er ikke gift og har ikke barn");
     }  
