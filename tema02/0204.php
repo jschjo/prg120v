3437c9d6ed55cf9552064fa6d15e7746d3a9e99b
jschjo <jschjonneberg@gmail.com>
2025-10-13 18:36:00 +0200

    .

--- tema02/0204.php
+++ tema02/0204.php
@@ -6,9 +6,9 @@
   $svar=$_POST ["svar"];
   $svar2=$_POST ["svar"];
 	
-  if (!$svar && !$svar2)  
+  if (!$svar || !$svar2)  
     {
-      print("Du har ikke svart &aring; sp&oslash;rsm&aring;lene om du er gift og har barn ");
+      print("Du har ikke svart p&aring; begge sp&oslash;rsm&aring;lene om du er gift og har barn ");
     }
   else if ($svar == "j" && $svar2 == "j")  
     {
@@ -21,10 +21,10 @@
   else if ($svar == "n" && $svar2 == "j")  
     {
       print("Du er ikke gift og har barn");
-    }
+    }   
   else if ($svar == "n" && $svar2 == "n")           
     {
       print("Du er ikke gift og har ikke barn");
     }  
      
-  ?>
+  ?>  
