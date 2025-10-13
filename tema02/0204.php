c381b80d5dd6b9c3ba0c037402aaf0cf8fc1b5ed
jschjo <jschjonneberg@gmail.com>
2025-10-13 19:10:52 +0200

    .

--- tema02/0204.php
+++ tema02/0204.php
@@ -1,34 +1,34 @@
-<?php     /* Eksempel 1 */
-/*
-/*    Programmet mottar fra et HTML-skjema et svar på spørsmålet "Hva er 3 ganger 3 ?"
-/*    Programmet sjekker om svaret er riktig og skriver ut en melding ang. svaret 
-*/
-  $gift=$_POST ["gift"];
-  $barn=$_POST ["barn"];
-	
-  if (!$gift || !$barn)  
-    {
-      print("Du har ikke svart p&aring; begge sp&oslash;rsm&aring;lene om du er gift og har barn ");
-    }
-  else if ($gift == "j" && $barn == "j")  
-    {
-      print(" Du er gift og har barn");
-    }   
-  else if ($gift == "j" && $barn == "n")  
-    {
-      print("Du er gift og har ikke barn");
-    }
-  else if ($gift == "n" && $barn == "j")  
-    {
-      print("Du er ikke gift og har barn");
-    }   
-  else if ($gift == "n" && $barn == "n")           
-    {
-      print("Du er ikke gift og har ikke barn");
-    }  
-      else  
-    {   
-      print("Begge sp&oslash;rsm&aring;lene har ikke gyldige svar");
-    }
-     
-  ?>  
+    <?php     /* Eksempel 1 */
+    /*
+    /*    Programmet mottar fra et HTML-skjema et svar på spørsmålet "Hva er 3 ganger 3 ?"
+    /*    Programmet sjekker om svaret er riktig og skriver ut en melding ang. svaret 
+    */
+      $gift=$_POST ["gift"];
+      $barn=$_POST ["barn"];
+      
+      if (!$gift || !$barn)  
+        {
+          print("Du har ikke svart p&aring; begge sp&oslash;rsm&aring;lene om du er gift og har barn ");
+        }
+      else if ($gift == "j" && $barn == "j")  
+        {
+          print(" Du er gift og har barn");
+        }   
+      else if ($gift == "j" && $barn == "n")  
+        {
+          print("Du er gift og har ikke barn");
+        }
+      else if ($gift == "n" && $barn == "j")  
+        {
+          print("Du er ikke gift og har barn");
+        }   
+      else if ($gift == "n" && $barn == "n")           
+        {
+          print("Du er ikke gift og har ikke barn");
+        }  
+          else  
+        {   
+          print("Begge sp&oslash;rsm&aring;lene har ikke gyldige svar");
+        }
+        
+      ?>  
