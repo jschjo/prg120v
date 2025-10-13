5def1c8aba836bc44c33db0f2cb1aa1df9d9f8d0
jschjo <jschjonneberg@gmail.com>
2025-10-13 20:42:30 +0200

    .

---
+++ tema02/0205.php
@@ -0,0 +1,36 @@
+    <?php     /* Eksempel 1 */
+    /*
+    /*    Programmet mottar fra et HTML-skjema et svar på spørsmålet "Hva er 3 ganger 3 ?"
+    /*    Programmet sjekker om svaret er riktig og skriver ut en melding ang. svaret 
+    */
+      $tall1=$_POST ["tall1"];
+      $tall2=$_POST ["tall2"];
+      $tall3=$_POST ["tall3"];
+
+      
+      if (!$gift || !$barn)  
+        {
+          print("Du har ikke svart p&aring; begge sp&oslash;rsm&aring;lene om du er gift og har barn ");
+        }
+      else if ($gift == "j" && $barn == "j")  
+        {
+          print(" Du er gift og har barn");
+        }   
+      else if ($gift == "j" && $barn == "n")  
+        {
+          print("Du er gift og har ikke barn");
+        }
+      else if ($gift == "n" && $barn == "j")  
+        {
+          print("Du er ikke gift og har barn");
+        }   
+      else if ($gift == "n" && $barn == "n")           
+        {
+          print("Du er ikke gift og har ikke barn");
+        }  
+          else  
+        {   
+          print("Begge sp&oslash;rsm&aring;lene har ikke gyldige svar");
+        }
+        
+      ?>  
