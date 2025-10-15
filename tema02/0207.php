a6bcdcf1bc9bd389609994e894208a79db732d8b
jschjo <jschjonneberg@gmail.com>
2025-10-15 19:05:59 +0200

    .

--- tema02/0207.php
+++ tema02/0207.php
@@ -16,67 +16,44 @@
           print("Det er ikke angitt en gyldig regneoperasjon");
         }
       else if ($tall1 < $tall2)   
-        {   
-
-          print ("Tall 1 er $tall1 <br/>"); 
-          print ("Tall 2 er $tall2 <br/>"); 
+        {  
           print ("Tall 1 er mindre enn Tall 2 <br/>");
         }                       
       else if ($tall1 == $tall2)   
         {
-        
-          print ("Tall 1 er $tall1 <br/>"); 
-          print ("Tall 2 er $tall2 <br/>");
           print ("Tall 1 er lik Tall 2 <br/>");
         }
       else if ($tall1 > $tall2)  
         {
-
-          print ("Tall 1 er $tall1 <br/>"); 
-          print ("Tall 2 er $tall2 <br/>");
           print ("Tall 1 er st&oslash;rre enn Tall 2<br/>");
         }
-
               else if ($tall1 < $tall3)   
         {   
 
-          print ("Tall 1 er $tall1 <br/>"); 
-          print ("Tall 3 er $tall3 <br/>"); 
           print ("Tall 1 er mindre enn Tall 3 <br/>");
         }                       
       else if ($tall1 == $tall3)   
         {
         
-          print ("Tall 1 er $tall1 <br/>"); 
-          print ("Tall 3 er $tall3 <br/>");
           print ("Tall 1 er lik Tall 3 <br/>");
         }
       else if ($tall1 > $tall3)  
         {
-
-          print ("Tall 1 er $tall1 <br/>"); 
-          print ("Tall 1 er $tall3 <br/>");
           print ("Tall 1 er st&oslash;rre enn Tall 3<br/>");
         } 
         else if ($tall2 < $tall3)   
         {   
 
-          print ("Tall 2 er $tall2 <br/>"); 
-          print ("Tall 3 er $tall3 <br/>"); 
-          print ("Tall 2 er mindre enn Tall 3 <br/>");
+           print ("Tall 2 er mindre enn Tall 3 <br/>");
         }                       
       else if ($tall2 == $tall3)   
         {
         
-          print ("Tall 2 er $tall2 <br/>"); 
-          print ("Tall 3 er $tall3 <br/>");
           print ("Tall 2 er lik Tall 3 <br/>");
         }
       else if ($tall2 > $tall3)  
         {
 
-          print ("Tall 2 er $tall2 <br/>"); 
-          print ("Tall 3 er $tall3 <br/>");
           print ("Tall 2 er st&oslash;rre enn Tall 3<br/>");
         }
 
