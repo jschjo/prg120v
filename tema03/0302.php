adcb7f1031046deda5873b75cd1bd88df402dd16
jschjo <jschjonneberg@gmail.com>
2025-10-19 01:30:34 +0200

    .

--- tema03/0302.php
+++ tema03/0302.php
@@ -6,8 +6,8 @@
 /*
 /* Programmet skriver ut tallene fra 1 til 10 på hver sin linje
 */
-for ($tall=1;$tall<=10;$tall++) /* repetisjon fra 1 til 10 */
+for ($tall=1;$tall*$tall1<10;$tall++) /* repetisjon fra 1 til 10 */
 {
-print("$tall har kvadratet ($tall*$tall) <br/>"); /* tallet skrevet ut */
+print("$tall har kvadratet $tall<br/>"); /* tallet skrevet ut */
 }
 ?>
