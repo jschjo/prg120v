b9119fa28f76003ca91f4c997fe76b4599d828b0
jschjo <jschjonneberg@gmail.com>
2025-10-18 17:10:40 +0200

    .

--- tema02/0208.php
+++ tema02/0208.php
@@ -1,16 +1,26 @@
-<?php     /* Oppgave 8 */
-/*
-/*    Programmet mottar fra et HTML-skjema et svar på spørsmålet "Hva er 3 ganger 3 ?"
-/*    Programmet sjekker om svaret er riktig og skriver ut en melding ang. svaret 
-*/
-  $svar=$_POST ["svar"];
-	
-  if ($svar == 9)  
-    {
-      print("Riktig. 3 ganger 3 er 9 ");
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Oppgave 8</title>
+</head>
+<body>
+  <h3>Oppgave 8</h3>
+    
+  <form method="post" action="" id="0208" name="0208">
+    Hva er 3 ganger 3 ? <input type="text" id="svar" name="svar" required  /> <br />
+    <input type="submit" value="Fortsett" id="fortsett" name="fortsett" />
+    <input type="reset" value="Nullstill" name="nullstill" id="nullstill" /> <br />
+  </form>
+</body>
+</html>
+
+<?php
+  if (isset($_POST ["fortsett"])) {
+    $svar=$_POST ["svar"];
+    if ($svar == 9){
+      print("Riktig. 3 ganger 3 er 9 <br/>");
+    } else {
+      print("Feil. 3 ganger 3 er ikke $svar. 3 ganger 3 er 9 <br/>");
     }
-  else  
-    {
-      print("Feil. 3 ganger 3 er ikke  $svar. 3 ganger 3 er 9 ");
-    }
-?>   
+  }
+?>
