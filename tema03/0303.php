60445c3d312c31f63a4682049236dda2bdff85ca
jschjo <jschjonneberg@gmail.com>
2025-10-19 01:55:54 +0200

    .

--- tema03/0303.php
+++ tema03/0303.php
@@ -2,13 +2,23 @@
 <html>
 <body>
 
-<?php /* Oppgave 2 */
+<?php /* Oppgave 3 */
 /*
 /* Programmet skriver ut tallene fra 1 til 10 på hver sin linje
 */
-for ($tall=1;$tall<=10;$tall++) /* repetisjon fra 1 til 10 */
+for ($tall=1;$tall<=10;$tall++)
 {
-$kvadrat=$tall*$tall;
-print("$tall har kvadratet $kvadrat<br/>"); /* tallet skrevet ut */
+//$kvadrat=$tall*$tall;
+print("$tall<br/>"); 
+}
+for ($tall=10;$tall<=20;$tall++)
+{
+//$kvadrat=$tall*$tall;
+print("$tall har kvadratet $kvadrat<br/>"); 
+}
+for ($tall=20;$tall<=30;$tall++)
+{
+//$kvadrat=$tall*$tall;
+print("$tall har kvadratet $kvadrat<br/>"); 
 }
 ?>
