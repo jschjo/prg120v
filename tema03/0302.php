99e2f73710630565d2ceb6af1dfa451fb2181edb
jschjo <jschjonneberg@gmail.com>
2025-10-19 01:18:16 +0200

    .

--- tema03/0302.php
+++ tema03/0302.php
@@ -2,12 +2,12 @@
 <html>
 <body>
 
-<?php /* Oppgave 1 */
+<?php /* Oppgave 2 */
 /*
 /* Programmet skriver ut tallene fra 1 til 10 på hver sin linje
 */
 for ($tall=1;$tall<=10;$tall++) /* repetisjon fra 1 til 10 */
 {
-print("$tall <br/>"); /* tallet skrevet ut */
+print("The number is: $tall <br/>"); /* tallet skrevet ut */
 }
 ?>
