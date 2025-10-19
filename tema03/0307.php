d8624d0f1f08880609b7737123530d830c7c2bc7
jschjo <jschjonneberg@gmail.com>
2025-10-19 15:43:04 +0200

    .

--- tema03/0307.php
+++ tema03/0307.php
@@ -7,8 +7,8 @@
   <h3>Oppgave 7</h3>
     
 <form method="post" action="" id="0307" name="0307">
- Skriv inn fornavn <input type="text" id="fornavn" name="fornavn" required  /> <br />
- Skriv inn etternavn <input type="text" id="etternavn" name="etternavn" required  /> <br />
+ Skriv inn fornavn <input type="text" id="navn" name="navn" required  /> <br />
+ Skriv inn etternavn <input type="text" id="navn" name="navn" required  /> <br />
     <input type="submit" value="Fortsett" id="fortsett" name="fortsett" />
     <input type="reset" value="Nullstill" name="nullstill" id="nullstill" /> <br />
    </form>
@@ -20,10 +20,11 @@
 /* Programmet mottar et navn fra et HTML-skjema
 /* Programmet skriver ut fornavn og etternavn hver for seg
 */
-$navn=$_POST["name"];
+$navn=$_POST["navn"];
 $oppdeltNavn=explode(" ",$navn);
 $fornavn=$oppdeltNavn[0];
 $etternavn=$oppdeltNavn[1];
 print("Fornavnet er $fornavn <br/>");
 print("Etternavnet er $etternavn <br/>"); /* fornavn og etternavn skrevet ut hver for seg */
 ?>
+
