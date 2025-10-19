88f2376f24df71e54b063db1d6273bdb5f21001c
jschjo <jschjonneberg@gmail.com>
2025-10-19 12:07:46 +0200

    .

---
+++ tema03/0305.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+ <body>
+
+<?php /* Oppgave 4 */
+/*
+/* Programmet skriver ut tallene fra 1 til 10 på hver sin linje
+ */
+
+$sum=0;
+ for ($tall=1;$tall<=10;$tall++)
+ {
+ $sum=$sum+$tall; 
+ }
+ print("Summenen av tallene fra 1 til 10 er $sum<br/>");
+ $gjennomsnitt=$sum*$tall/10; 
+ {
+ print("Gjennomsnitt av tallene fra 1 til 10 er $gjennomsnitt<br/>");
+ }
+ ?> 
