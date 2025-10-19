18a60c8fb2c22cb9aaee7fb986f66b41e51b9ab7
jschjo <jschjonneberg@gmail.com>
2025-10-19 12:01:57 +0200

    .

--- tema03/0304.php
+++ tema03/0304.php
@@ -1,16 +1,16 @@
 <!DOCTYPE html>
 <html>
-<body>
+ <body>
 
 <?php /* Oppgave 4 */
 /*
 /* Programmet skriver ut tallene fra 1 til 10 på hver sin linje
-*/
+ */
 
 $sum=0;
-for ($tall=1;$tall<=10;$tall++)
-{
-$sum=$sum+$tall; 
-}
-print("Summenen av tallene fra 1 til 10 er $sum<br/>");
-?>
+ for ($tall=1;$tall<=10;$tall++)
+ {
+ $sum=$sum+$tall; 
+ }
+ print("Summenen av tallene fra 1 til 10 er $sum<br/>");
+ ?>
