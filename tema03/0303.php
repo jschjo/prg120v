cbb56d6491a7201c1ed5061f6f9c210e1d2f9311
jschjo <jschjonneberg@gmail.com>
2025-10-19 02:02:12 +0200

    .

--- tema03/0303.php
+++ tema03/0303.php
@@ -9,13 +9,15 @@
 for ($tall=1;$tall<=10;$tall++)
 {
 print("$tall"); 
+print ("<br/>");
 }
 for ($tall=10;$tall<=20;$tall++)
 {
 print("$tall"); 
+print ("<br/>");
 }
 for ($tall=20;$tall<=30;$tall++)
 {
-print("$tall"); 
+print("$tall");
 }
 ?>
