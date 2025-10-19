f056268ad189b4244cacc9eccc16a69fc0cf1ee1
jschjo <jschjonneberg@gmail.com>
2025-10-19 16:12:58 +0200

    .

--- tema03/0307.php
+++ tema03/0307.php
@@ -1,19 +1,11 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>Oppgave 7  </title>
+  <title>Oppgave 7 </title>
 </head>
 <body>
   <h3>Oppgave 7</h3>
     
-<form method="post" action="" id="0307" name="0307">
- Skriv inn fornavn <input type="text" id="navn" name="navn" required  /> <br />
- Skriv inn etternavn <input type="text" id="navn" name="navn" required  /> <br />
-    <input type="submit" value="Fortsett" id="fortsett" name="fortsett" />
-    <input type="reset" value="Nullstill" name="nullstill" id="nullstill" /> <br />
-   </form>
-</body>
-</html>
 
 <?php /* Oppgave 7 */
 /*
