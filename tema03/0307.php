0791d76f763b73afbf4ff8f30e89fa554a142824
jschjo <jschjonneberg@gmail.com>
2025-10-19 16:18:35 +0200

    .

--- tema03/0307.php
+++ tema03/0307.php
@@ -12,6 +12,8 @@
 /* Programmet mottar et navn fra et HTML-skjema
 /* Programmet skriver ut fornavn og etternavn hver for seg
 */
+
+
 $navn=$_POST["navn"];
 $oppdeltNavn=explode(" ",$navn);
 $fornavn=$oppdeltNavn[0];
