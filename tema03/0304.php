57f88cfb8d2164f0f3bc8b30c84a474cb955f86c
jschjo <jschjonneberg@gmail.com>
2025-10-19 11:28:56 +0200

    .

---
+++ tema03/0304.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php /* Oppgave 3 */
+/*
+/* Programmet skriver ut tallene fra 1 til 10 på hver sin linje
+*/
+for ($tall=1;$tall<=10;$tall++)
+{
+print("$tall"); 
+}
+print("<br/>");
+
+for ($tall=11;$tall<=20;$tall++)
+{
+print("$tall"); 
+}
+print("<br/>");
+
+for ($tall=21;$tall<=30;$tall++)
+{
+print("$tall");
+}
+print("<br/>");
+?>
