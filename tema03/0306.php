e46183c0e81b86ca0624b6ec5b46b41c719a959c
jschjo <jschjonneberg@gmail.com>
2025-10-19 22:03:44 +0200

    .

--- tema03/0306.php
+++ tema03/0306.php
@@ -1,19 +1,3 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Oppgave 6  </title>
-</head>
-<body>
-  <h3>Oppgave 6</h3>
-    
-  <form method="post" action="" id="0306" name="0306">
-    Skriv inn et postivt heltall <input type="text" id="svar" name="svar" required  /> <br />
-    <input type="submit" value="Fortsett" id="fortsett" name="fortsett" />
-    <input type="reset" value="Nullstill" name="nullstill" id="nullstill" /> <br />
-  </form>
-</body>
-</html>
-
 
 <?php /* Oppgave 6 */
 /*
