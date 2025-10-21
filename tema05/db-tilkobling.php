7b8ece7584df1832acf84b96541131265eef84bc
jschjo <jschjonneberg@gmail.com>
2025-10-21 17:59:24 +0200

    .

--- tema05/db-tilkobling.php
+++ tema05/db-tilkobling.php
@@ -6,6 +6,7 @@ $host = getenv('DB_HOST');
 $username = getenv('DB_USER');
 $password = getenv('DB_PASSWORD');
 $database = getenv('DB_DATABASE');
+
 $db=mysqli_connect($host,$username,$password,$database) or die ("ikke kontakt med database-server");
 /* tilkobling til database-serveren utført */
 ?>
