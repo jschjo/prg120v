1ce3cdfc63756a1908d33d9b8b73d94f95673391
jschjo <jschjonneberg@gmail.com>
2025-10-23 23:58:29 +0200

    .

--- tema05-06/vis-alle-klasser.php
+++ tema05-06/vis-alle-klasser.php
@@ -11,7 +11,7 @@
 SQL-setning sendt til database-serveren */ 
   $antallRader=mysqli_num_rows($sqlResultat);  /* antall rader i resultatet beregnet */ 
  
-  print ("<h3>Registrerte klsddr </h3>");  
+  print ("<h3>Registrerte klasser </h3>");  
   print ("<table border=1>"); 
   print ("<tr><th align=left>klassekode</th> <th align=left>klassenavn</th> <th align=left>studiumkode</th> 
 </tr>"); 
@@ -19,8 +19,8 @@ SQL-setning sendt til database-serveren */
   for ($r=1;$r<=$antallRader;$r++) 
     { 
       $rad=mysqli_fetch_array($sqlResultat);  /* ny rad hentet fra spørringsresultatet */ 
-      $emnekode=$rad["klassekode"];  
-      $emnenavn=$rad["klassenavn"];  
+      $klassekode=$rad["klassekode"];  
+      $klassenavn=$rad["klassenavn"];  
       $studiumkode=$rad["studiumkode"]; 
  
       print ("<tr> <td> $klassekode </td> <td> $klassenavn </td> <td> $studiumkode </td> </tr>");   
