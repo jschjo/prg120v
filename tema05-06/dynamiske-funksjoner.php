a6338733627dfc6ecf61bd44dbd6b17f61695607
jschjo <jschjonneberg@gmail.com>
2025-10-24 00:39:54 +0200

    .

--- tema05-06/dynamiske-funksjoner.php
+++ tema05-06/dynamiske-funksjoner.php
@@ -62,9 +62,9 @@ function listeboksKlassekode ()
       $rad=mysqli_fetch_array($sqlResultat);  /* ny rad hentet fra spørringsresultatet */ 
       $emnekode=$rad["klassekode"];     
       $emnenavn=$rad["klassenavn"];         
-      $studiumkode=$rad["studiumkode"];  
+      $studiumkode=$rad["klassekode"];  
     
-      print("<option value='$klassekode'>$klassenavn $studiumkode </option>");  /* ny verdi i listeboksen laget 
+      print("<option value='$klassekode'>$klassenavn $klassekode </option>");  /* ny verdi i listeboksen laget 
 */ 
     } 
 } 
