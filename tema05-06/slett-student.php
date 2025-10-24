3e116967664013361ea0a03f4bbdced315a8ee15
jschjo <jschjonneberg@gmail.com>
2025-10-24 15:54:33 +0200

    .

--- tema05-06/slett-student.php
+++ tema05-06/slett-student.php
@@ -26,10 +26,10 @@ print ("Det er ikke valgt noen student");
 else
 {
 include("db-tilkobling.php"); /* tilkobling til database-serveren utført og valg av database foretatt */
-$sqlSetning="DELETE FROM brukernavn WHERE brukernavn='$brukernavn';";
+$sqlSetning="DELETE FROM student WHERE brukernavn='$brukernavn';";
 mysqli_query($db,$sqlSetning) or die ("ikke mulig &aring; slette data i databasen");
 /* SQL-setning sendt til database-serveren */
-print ("F&oslash;lgende emne er n&aring; slettet: $brukernavn <br />");
+print ("F&oslash;lgende student er n&aring; slettet: $brukernavn <br />");
 }
 }
 ?>
