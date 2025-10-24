100b8e2d7313c26b880b60e7f449810209c6bf04
jschjo <jschjonneberg@gmail.com>
2025-10-24 03:06:59 +0200

    .

--- tema05-06/dynamiske-funksjoner.php
+++ tema05-06/dynamiske-funksjoner.php
@@ -69,5 +69,27 @@ function listeboksKlassekode ()
 */ 
     } 
 } 
+
+function listeboksStudentkode () 
+{ 
+  include("db-tilkobling.php");  /* tilkobling til database-server og valg av database utført */ 
+       
+  $sqlSetning="SELECT * FROM student ORDER BY brukernavn;"; 
+  $sqlResultat=mysqli_query($db,$sqlSetning) or die ("ikke mulig &aring; hente data fra databasen");  
+       
+  $antallRader=mysqli_num_rows($sqlResultat);  /* antall rader i resultatet beregnet */ 
+ 
+  for ($r=1;$r<=$antallRader;$r++) 
+    { 
+      $rad=mysqli_fetch_array($sqlResultat);  /* ny rad hentet fra spørringsresultatet */ 
+      $brukernavn=$rad["brukernavn"];     
+      $fornavn=$rad["fornavn"];         
+      $etternavn=$rad["etternavn"];
+      $klassekode=$rad["klassekode"];
+    
+      print("<option value='$brukernavn'>$fornavnkode $etternavn $klassekode </option>");  /* ny verdi i listeboksen laget 
+*/ 
+    } 
+} 
  
 ?> 
